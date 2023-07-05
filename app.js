7c1f5884c0d67b5889f51a5a76f8b985725757bf
bezawork <bezaworkalx@gmail.com>
2023-07-05 13:25:53 +0300

    Create front end with bootstrap

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const userRouter = require('./routes/users')
+const userRouter = require("./routes/users");
+
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.use('/users', userRouter)
+app.use("/users", userRouter);
 app.listen(port);
