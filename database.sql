42f3860b4c97c83570e2befcc59e8e32852d55a0
bezawork <bezaworkalx@gmail.com>
2023-07-13 16:45:56 +0300

    Add bootstrap features and routes to handle different requests

--- database.sql
+++ database.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE Eat_Smart_Shop;
 USE Eat_Smart_Shop;
 CREATE TABLE User (
-  PersonId int NOT NULL,
+  PersonId int NOT NULL ,
   FirstName varchar(255) NOT NULL,
   LastName varchar(255) NOT NULL,
   ProductDict JSON,
   OrderId int NOT NULL,
+  PRIMARY KEY (PersonId),
+  UNIQUE (PersonId),
   FOREIGN KEY (OrderId) REFERENCES Orders(OrderId)
 );
 -- CREATE TABLE TotalPurchase (
@@ -20,6 +22,8 @@ CREATE TABLE User (
 CREATE TABLE Product (
   ProductId int NOT NULL,
   ProductName varchar(255) NOT NULL,
+  ProductDescription varchar(255) NOT NULL,
+  Price int NOT NULL,
   InitialQuantity int NOT NULL,
   HowManyLeft int NOT NULL,
   PRIMARY KEY (ProductId)
@@ -35,23 +39,29 @@ CREATE TABLE Orders (
 );
 
 
-INSERT INTO Product (ProductId, ProductName, InitialQuantity, HowManyLeft)
-VALUES (1, "Iphone", 20, 20);
+INSERT INTO Product (ProductId, ProductName, ProductDescription, Price, InitialQuantity, HowManyLeft)
+VALUES (1, "Iphone", "Iphone is a great quality product BUY one TODAY!!!", 100, 20, 20);
 
-INSERT INTO Product (ProductId, ProductName, InitialQuantity, HowManyLeft)
-VALUES (2, "Samsung", 50, 50);
+INSERT INTO Product (ProductId, ProductName, ProductDescription, Price, InitialQuantity, HowManyLeft)
+VALUES (2, "Samsung", "Samsung is a great quality product BUY one TODAY!!!", 80, 50, 50);
 
-INSERT INTO Product (ProductId, ProductName, InitialQuantity, HowManyLeft)
-VALUES (3, "Vodaphone", 100, 100);
+INSERT INTO Product (ProductId, ProductName, ProductDescription, Price, InitialQuantity, HowManyLeft)
+VALUES (3, "Vodaphone", "Vodaphone is a great quality product BUY one TODAY!!!", 60, 100, 100);
 
-INSERT INTO Product (ProductId, ProductName, InitialQuantity, HowManyLeft)
-VALUES (4, "Sony", 20, 20);
+INSERT INTO Product (ProductId, ProductName, ProductDescription, Price, InitialQuantity, HowManyLeft)
+VALUES (4, "Sony", "Sony is a great quality product BUY one TODAY!!!", 100, 20, 20);
 
-INSERT INTO Product (ProductId, ProductName, InitialQuantity, HowManyLeft)
-VALUES (5, "Google", 40, 40);
+INSERT INTO Product (ProductId, ProductName, ProductDescription, Price, InitialQuantity, HowManyLeft)
+VALUES (5, "Google", "Google is a great quality product BUY one TODAY!!!", 90, 40, 40);
 
-INSERT INTO Product (ProductId, ProductName, InitialQuantity, HowManyLeft)
-VALUES (6, "Xperia", 35, 35);
+INSERT INTO Product (ProductId, ProductName, ProductDescription, Price, InitialQuantity, HowManyLeft)
+VALUES (6, "Xperia", "Xperia is a great quality product BUY one TODAY!!!", 70, 35, 35);
+
+INSERT INTO Product (ProductId, ProductName, ProductDescription, Price, InitialQuantity, HowManyLeft)
+VALUES (7, "Nokia", "Nokia is a great quality product BUY one TODAY!!!", 78, 39, 39);
+
+INSERT INTO Product (ProductId, ProductName, ProductDescription, Price, InitialQuantity, HowManyLeft)
+VALUES (8, "Techno", "Techno is a great quality product BUY one TODAY!!!", 40, 100, 100);
 
 INSERT INTO Orders (OrderId, HowManyOrdered, ProductId, OrderReceipt)
 VALUES(1, 1, 1, 1);
--- index.js
+++ index.js
@@ -2,17 +2,127 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 
-const mysql = require("mysql");
-mysql.createConnection({
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "Eat_Smart_Shop",
 });
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Database Connected");
+});
+try {
+  const order =
+    "INSERT INTO Orders (OrderId, HowManyOrdered, ProductId, OrderReceipt) VALUES(1, 1, 1, 1);";
+  connection.query(order, (err, result) => {
+    console.log("Order added");
+  });
+} catch (err) {
+  console.log(err);
+}
 const app = express();
 const PORT = 5000;
+let currentUserFirstName = "Unknown";
+let currentUserLastName = "UnKnown";
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/", (req, res) => res.render("pages/index"));
+// app.use(function (req, res, next) {
+//   console.log("Connected");
+//   res.locals.connection = createConnection;
+//   console.log("Disconnected");
+//   next();
+// });
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM Product", (err, result) => {
+    res.render("pages/index", { result: result });
+  });
+});
+
+app.get("/registration", (req, res) => {
+  res.render("pages/registration");
+});
+app.post("/registration", (req, res) => {
+  let firstName = req.body.firstname;
+  let lastName = req.body.lastname;
+  const PersonId = 1;
+  connection.query(
+    "INSERT INTO User (PersonId, FirstName, LastName, OrderId) VALUES (1, ?, ?, 1) ",
+    [firstName, lastName],
+    (err, result) => {
+      if (err) throw err;
+      console.log("Registered");
+    }
+  );
+  res.render("pages/registration");
+});
+app.get("/login", (req, res) => {
+  res.render("pages/login");
+});
+app.post("/login", (req, res) => {
+  const firstName = req.body.firstname;
+  const lastName = req.body.lastname;
+  let currentUserFirstName = firstName;
+  let currentUserLastName = lastName;
+  let result = 1;
+  connection.query("SELECT * FROM Product", (err, res) => {
+    result = res;
+  });
+  connection.query("SELECT * FROM User", (err, Userresult) => {
+    for (let i in Userresult) {
+      if (
+        Userresult[i].FirstName === firstName &&
+        Userresult[i].LastName === lastName
+      ) {
+        res.render("pages/logged", {
+          result: result,
+          firstName: firstName,
+          lastName: lastName,
+        });
+      }
+    }
+  });
+});
+app.get("/buy", (req, res) => {
+  let result = 1;
+  connection.query("SELECT * FROM Product", (err, res) => {
+    result = res;
+  });
+  connection.query("SELECT * FROM User", (err, Userresult) => {
+    for (let i in Userresult) {
+      if (
+        Userresult[i].FirstName === currentUserFirstName &&
+        Userresult[i].LastName === currentUserLastName
+      ) {
+        res.render("pages/buy", {
+          result: result,
+          firstName: currentUserFirstName,
+          lastName: currentUserLastName,
+        });
+      }
+    }
+    res.render("pages/buy", {
+      result: result,
+      firstName: currentUserFirstName,
+      lastName: currentUserLastName,
+    });
+  });
+});
+app.post("/buy", (req, res) => {
+  let products = 1
+  connection.query("SELECT * FROM Product", (err, res) => {
+    result = res;
+  });
+  for (let i in result) {
+    
+  }
+  const updateProductQuery = "UPDATE Orders set"
+});
+
 app.listen(PORT);
